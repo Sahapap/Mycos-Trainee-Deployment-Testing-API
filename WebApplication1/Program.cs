301c8b86e7dc15d2b781460e2d3e371c3902795e
Sahapap <sahapap_panya@cmu.ac.th>
2024-04-15 12:11:04 +0700

    Add CORS

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using WebApplication1.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 // Add services to the container.
 
@@ -9,9 +10,19 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<FoodContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:5173");
+                      });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseHttpsRedirection();
 
